a298be6c53de60e15b6bca5bef7718f63a289444
yunjey0 <sehajyang@gmail.com>
2018-10-17 23:03:26 +0900

    Edit style/*, common/Header/*

--- src/components/common/PageTemplate/PageTemplate.js
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
+import Header from 'components/common/Header';
+
+//컴포넌트를 랜더링함
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = () => (
     <div className={cx('page-template')}>
-    PageTemplate
+    <Header/>
     </div>
 );
 
