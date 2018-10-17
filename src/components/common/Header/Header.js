ec48b2ecc20540aa7f9c76e062b37b51f9f03777
yunjey0 <sehajyang@gmail.com>
2018-10-17 22:46:49 +0900

    Create component/*, styles/* dir

---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Header.scss';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+const Header = () => (
+  <header className={cx('header')}>
+    <div className={cx('header-content')}>
+      <div className={cx('brand')}>
+        <Link to="/">schedule</Link>
+      </div>
+      <div className={cx('right')}>
+        {/**/}
+        오른쪽
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
