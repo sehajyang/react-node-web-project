e75afaa39dcbb70d7ae32324bbf6f4f80f869dbc
yunjey0 <sehajyang@gmail.com>
2018-10-19 18:11:10 +0900

    Add paging option

--- src/components/list/Pagination/Pagination.js
+++ src/components/list/Pagination/Pagination.js
@@ -1,25 +1,28 @@
 import React from 'react';
-
 import styles from './Pagination.scss';
 import classNames from 'classnames/bind';
 import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
+const Pagination = ({page, lastPage, tag}) => {
+  const createPagePath = (page) => {
+    return tag ? `/tag/${tag}/${page}` : `/page/${page}`;
+  }
 
-const Pagination = () => (
-  <div className={cx('pagination')}>
-    <Button disabled>
-    이전
-    </Button>
-    <div className={cx('number')}>
-    페이지 1
+  return (
+    <div className={cx('pagination')}>
+      <Button disabled={page === 1} to={createPagePath(page -1)}>
+        이전 페이지
+      </Button>
+      <div className={cx('number')}>
+        페이지 {page}
+      </div>
+      <Button disabled={page===lastPage-1} to={createPagePath(page+1)}>
+        다음 페이지
+      </Button>
     </div>
-    <Button>
-      다음페이지
-    </Button>
-  </div>
-);
-
+  );
+};
 
 export default Pagination;
